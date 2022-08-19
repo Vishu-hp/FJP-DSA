059153752866b963838d2aab59764b6881a2e9e4
DOM-Vishu <70435386+Vishu-hp@users.noreply.github.com>
2022-08-19 23:39:57 +0530

    Update countOfSubarraysWithEqualNumberOfZeroesAndOnes.cpp

--- Level 2/Hashmap/countOfSubarraysWithEqualNumberOfZeroesAndOnes.cpp
+++ Level 2/Hashmap/countOfSubarraysWithEqualNumberOfZeroesAndOnes.cpp
@@ -1,6 +1,7 @@
 #include "bits/stdc++.h"
 using namespace std;
 
+// METHOD-1
 int solution(vector<int> &v){
     //Write your code here.
     int n = v.size();
@@ -27,7 +28,36 @@ int solution(vector<int> &v){
     }
     return count;
 }
+// ***************************************************************************************************
+// METHOD 2
+int solution(vector<int> &v){
+    //Write your code here.
+    int n = v.size();
 
+    int numZero=0 , numOne=0;
+    int ans=0;
+    unordered_map<int,int>mp;
+    mp[0]=1;
+    for(int i=0;i<n;i++){
+        if(v[i]==0){
+            numZero++;
+        }
+        else{
+            numOne++;
+        }
+        int diff = numZero-numOne;
+        if(mp.count(diff)>0){
+            ans += mp[diff];
+            mp[diff]++;
+        }
+        else{
+            mp[diff]=1;
+        }
+    }
+    return ans;
+}
+
+// ****************************************************************************************************************
 
 int main(){
     int n;cin>> n;
