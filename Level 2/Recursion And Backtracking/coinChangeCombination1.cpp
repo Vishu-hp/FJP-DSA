185d1e7ca00c77217141e2ba28af9a821e354c17
DOM-Vishu <70435386+Vishu-hp@users.noreply.github.com>
2022-09-26 22:12:01 +0530

    Create coinChangeCombination1.cpp

---
+++ Level 2/Recursion And Backtracking/coinChangeCombination1.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+void coinChange(vector<int>coins,int idx,int amtsf,int tamt,string asf){
+    //write your code here
+    if(idx == coins.size()){
+        if(amtsf == tamt){
+            cout<<asf+"."<<endl;
+        }
+        return;
+    }
+    
+    coinChange(coins,idx+1,amtsf+coins[idx],tamt,asf+to_string(coins[idx])+"-");
+    coinChange(coins,idx+1,amtsf,tamt,asf);
+}
+int main(){
+    int n;
+    cin>>n;
+    vector<int>coins(n);
+    for(int i=0;i<n;i++){
+        cin>>coins[i];
+    }
+    int amt;
+    cin>>amt;
+   
+    coinChange(coins,0,0,amt,"");
+}
