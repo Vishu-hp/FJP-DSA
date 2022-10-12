78757d3e00ee1a127840e2a486a9683ab1ac1bec
DOM-Vishu <70435386+Vishu-hp@users.noreply.github.com>
2022-10-12 22:43:33 +0530

    Update and rename a.cpp to isEven.cpp

---
+++ Level 2/Bit Manipulation/isEven.cpp
@@ -0,0 +1,10 @@
+void evenOdd(int a, int b){
+        // Code here
+        int mask=1;
+        if(a&mask > 0){
+            cout<<b<<endl<<a<<endl;
+        }
+        else{
+            cout<<a<<endl<<b<<endl;
+        }
+}
