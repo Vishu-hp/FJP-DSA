1a973b2fd266dd65e8ec6209c2c457927d7e7b70
DOM-Vishu <70435386+Vishu-hp@users.noreply.github.com>
2022-10-14 22:18:54 +0530

    Create getConnectedComponentsOfAGraph.cpp

---
+++ Level 2/Graph/Revision Level 1/getConnectedComponentsOfAGraph.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Edge {
+public:
+  int src;
+  int nbr;
+  int wt;
+
+  Edge(int src, int nbr, int wt) {
+    this->src = src;
+    this->nbr = nbr;
+    this->wt = wt;
+  }
+};
+
+void dfs(vector<Edge>g[],vector<bool>&vis,int src,vector<int>&comp){
+    vis[src]=true;
+    comp.push_back(src);
+    for(auto x:g[src]){
+        if(vis[x.nbr]==false){
+            dfs(g,vis,x.nbr,comp);
+        }
+    }
+}
+
+int main() {
+  int vtces;
+  cin >> vtces;
+  vector<Edge>graph[vtces];
+
+  int edges;
+  cin >> edges;
+  for (int i = 0; i < edges; i++) {
+    int v1 ;
+    int v2 ;
+    int wt ;
+    cin >> v1 >> v2 >> wt;
+    graph[v1].push_back( Edge(v1, v2, wt));
+    graph[v2].push_back( Edge(v2, v1, wt));
+  }
+
+
+  vector<vector<int>> comps;
+
+  // write your code here
+    vector<bool>vis(vtces,false);
+    
+    for(int i=0;i<vtces;i++){
+        if(vis[i]==false){
+            vector<int>comp;
+            dfs(graph,vis,i,comp);
+            comps.push_back(comp);
+        }
+    }
+
+  cout << "[";
+  for (int i = 0 ; i < comps.size() ; i++) {
+    cout << "[";
+    for (int j = 0 ; j < comps[i].size() ; j++) {
+      if (j != comps[i].size() - 1)
+        cout << comps[i][j] << ", ";
+      else
+        cout << comps[i][j];
+
+    }
+    cout << "]";
+    if (i != comps.size() - 1)cout << ", ";
+  }
+  cout << "]";
+}
