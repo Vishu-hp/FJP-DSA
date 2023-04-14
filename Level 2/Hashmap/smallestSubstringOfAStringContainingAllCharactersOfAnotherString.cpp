922ed00837e0a323395d53b0f622b9d2e30337a0
DOM-Vishu <70435386+Vishu-hp@users.noreply.github.com>
2023-04-14 15:28:36 +0530

    Update smallestSubstringOfAStringContainingAllCharactersOfAnotherString.cpp

--- Level 2/Hashmap/smallestSubstringOfAStringContainingAllCharactersOfAnotherString.cpp
+++ Level 2/Hashmap/smallestSubstringOfAStringContainingAllCharactersOfAnotherString.cpp
@@ -1,6 +1,6 @@
 // LEETCODE ----- 76. Minimum Window Substring
 
-
+// OLD WAY (GIVES TLE NOW)
 class Solution {
 public:
     string minWindow(string s1, string s2) {
@@ -36,5 +36,52 @@ public:
 		j++;
 	}
 	return ans;
+    }
+};
+
+
+// *************************************************************************************************************
+// NEW WAY
+
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        unordered_map<char,int>ump;
+        for(auto x:t){
+            ump[x]++;
+        }
+        int ans=INT_MAX;
+        int cnt = ump.size();
+        
+        int i=0,j=0,start;
+        while(j<s.length()){
+            ump[s[j]]--;
+            
+            if(ump[s[j]] == 0){
+                cnt--;
+            }
+            
+            if(cnt==0){
+                while(cnt==0){
+                    if(ans > j-i+1){
+                        ans = j-i+1;
+                        start=i;
+                    }
+                        
+                    ump[s[i]]++;
+                    if(ump[s[i]]>0){
+                        cnt++;
+                    }
+                    i++;
+                }
+            }
+            j++;
+        }
+        
+        if(ans!=INT_MAX){
+            return s.substr(start,ans);
+        }
+        
+        return "";
     }
 };
