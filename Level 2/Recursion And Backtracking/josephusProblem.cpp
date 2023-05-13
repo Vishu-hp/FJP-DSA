2b54e0b429cf38b7934a00f9c194da247ca30fd0
DOM-Vishu <70435386+Vishu-hp@users.noreply.github.com>
2023-05-13 15:48:49 +0530

    Update josephusProblem.cpp

--- Level 2/Recursion And Backtracking/josephusProblem.cpp
+++ Level 2/Recursion And Backtracking/josephusProblem.cpp
@@ -1,3 +1,4 @@
+// NADOS SOLUTION
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -18,3 +19,24 @@ int main(){
     cout<<solve(n,k)<<endl;
     return 0;
 }
+
+
+// GFG SOLUTION(QUESTION STATEMENT IS A LITTLE DIFFERENT (0 TO N-1) AND (1 TO N)
+class Solution
+{
+    public:
+    int josephus(int n, int k)
+    {
+       //Your code here
+       if(n==1){
+           return 1;
+       }
+       
+       int x = josephus(n-1,k);
+       int ans = (x+k)%n;
+       if(ans==0){
+           return n;
+       }
+       return ans;
+    }
+};
