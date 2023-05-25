ddb49dc75e65e44b921cc56665cda59b8f623f37
DOM-Vishu <70435386+Vishu-hp@users.noreply.github.com>
2023-05-25 12:33:07 +0530

    Create printZigZag.cpp

---
+++ Level-1/Recursion/printZigZag.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+
+void pzz(int n){
+    // write your code here
+    if(n==0){
+      return;
+    }
+    
+    cout<<n<<" ";   
+    pzz(n-1);         
+    cout<<n<<" ";       
+    pzz(n-1);           
+    cout<<n<<" ";         
+    return;
+}
+
+
+
+int main(){
+    int n; cin>>n;
+    pzz(n);
+}
