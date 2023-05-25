5906f5f7d732b5ddc537433dfdd693b32d9e5ae9
DOM-Vishu <70435386+Vishu-hp@users.noreply.github.com>
2023-05-25 12:36:18 +0530

    Create firstIndex.cpp

---
+++ Level-1/Recursion/firstIndex.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int firstIndex(int arr[], int idx, int x, int n){
+    // write your code here
+    if(idx==n){
+        return -1;
+    }
+    int faith = firstIndex(arr,idx+1,x,n);
+    if(arr[idx]==x){
+        return idx;
+    }
+    return faith;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int d;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    cin >> d;
+    int p = firstIndex(arr, 0, d, n);
+    cout << p << endl;
+}
