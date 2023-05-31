387bb7bdf704f520b29526412d4a47134d9bf3e1
DOM-Vishu <70435386+Vishu-hp@users.noreply.github.com>
2023-05-31 20:20:45 +0530

    Update 1094CarPooling.cpp

--- Level 2/Array And Strings/1094CarPooling.cpp
+++ Level 2/Array And Strings/1094CarPooling.cpp
@@ -1,5 +1,6 @@
 // https://leetcode.com/problems/car-pooling/
 
+// RANGE ADDITION APPLICATION SOLUTION
 class Solution {
 public:
     bool carPooling(vector<vector<int>>& trips, int capacity) {
@@ -27,6 +28,59 @@ public:
             if(pSum>capacity){
                 return false;
             }
+        }
+        return true;
+    }
+};
+
+// GENERAL WAY COMES TO MIND
+class Solution {
+public:
+    static bool comp(vector<int>&a, vector<int>&b){
+        return a[1] < b[1];
+    }
+
+    class Pair{
+        public:
+        int ep;
+        int pas;
+
+        Pair(){}
+
+        Pair(int ep, int pas){
+            this->ep = ep;
+            this->pas =pas;
+        }
+    };
+
+    class Comp{
+        public:
+        bool operator()(Pair &a, Pair &b){
+            return a.ep > b.ep;
+        }
+    };
+
+    bool carPooling(vector<vector<int>>& trips, int capacity) {
+        sort(trips.begin(), trips.end(), comp);
+        priority_queue<Pair, vector<Pair>, Comp>pq;
+        int cp=0;
+        
+        for(int i=0;i<trips.size();i++){
+            int p=trips[i][0];
+            int f = trips[i][1];
+            int t = trips[i][2];
+
+            while(pq.size()>0 && pq.top().ep <= f){
+                cp -= pq.top().pas;
+                pq.pop();
+            }
+            
+
+            cp += p;
+            if(cp>capacity){
+                return false;
+            }
+            pq.push(Pair(t,p));
         }
         return true;
     }
