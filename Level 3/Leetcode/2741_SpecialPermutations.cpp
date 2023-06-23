fb5daa02c7dc82553a0c60413d95421e5e7052db
DOM-Vishu <70435386+Vishu-hp@users.noreply.github.com>
2023-06-23 15:54:01 +0530

    Update 2741_SpecialPermutations.cpp

--- Level 3/Leetcode/2741_SpecialPermutations.cpp
+++ Level 3/Leetcode/2741_SpecialPermutations.cpp
@@ -3,6 +3,8 @@
 // For all indexes 0 <= i < n - 1, either nums[i] % nums[i+1] == 0 or nums[i+1] % nums[i] == 0.
 // Return the total number of special permutations. As the answer could be large, return it modulo 109 + 7.
 
+// DP WITH BITMASK PRACTICE PROBLEM
+
 
 class Solution {
 public:
