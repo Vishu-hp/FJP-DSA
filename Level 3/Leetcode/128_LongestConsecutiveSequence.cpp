f1341320345b84233ace94a0b51cac9dabfeb133
DOM-Vishu <70435386+Vishu-hp@users.noreply.github.com>
2023-06-24 17:01:18 +0530

    Update 128_LongestConsecutiveSequence.cpp

--- Level 3/Leetcode/128_LongestConsecutiveSequence.cpp
+++ Level 3/Leetcode/128_LongestConsecutiveSequence.cpp
@@ -28,3 +28,75 @@ public:
         return ans;
     }
 };
+
+// SOLVED USING DSU
+class Solution {
+public:
+    vector<int>par;
+    vector<int>rank;
+
+    int findPar(int n){
+        if(par[n] == n){
+            return n;
+        }
+
+        int p = findPar(par[n]);
+        par[n] = p;
+        return p;
+    }
+
+    void union_(int a,int b){
+        int p1 = findPar(a);
+        int p2 = findPar(b);
+
+        if(p1==p2){
+            return;
+        }
+
+        if(rank[p1]>rank[p2]){
+            par[p2] = p1;
+            rank[p1] += rank[p2];
+        }
+        else{
+            par[p1] = p2;
+            rank[p2] += rank[p1];
+        }
+    }
+
+    int longestConsecutive(vector<int>& nums) {
+        int n = nums.size();
+        par.resize(n);
+        rank.resize(n);
+
+        for(int i=0;i<n;i++){
+            par[i] = i;
+            rank[i] = 1;    
+        }
+
+        unordered_map<int,int>ump;
+
+        for(int i=0;i<n;i++){
+            int num = nums[i];
+
+            if(ump.find(num)!=ump.end()){
+                continue;
+            }
+
+            ump[num] = i;
+
+            if(ump.find(num+1) != ump.end()){
+                union_(i,ump[num+1]);
+            }
+
+            if(ump.find(num-1) != ump.end()){
+                union_(i,ump[num-1]);
+            }
+        }
+
+        int ans=0;
+        for(int i=0;i<n;i++){
+            ans =max(ans,rank[i]);
+        }
+        return ans;
+    }
+};
